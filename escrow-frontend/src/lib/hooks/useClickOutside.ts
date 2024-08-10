234e5e26b69770d2a69179d897786be041d44aad
Olatunde Adegboyebo <tundx0@gmail.com>
2024-08-10 06:28:14 +0100

    add hook to enhance close on outside click behaviour

---
+++ escrow-frontend/src/lib/hooks/useClickOutside.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from "react";
+
+export const useClickOutside = <T extends HTMLElement>(
+  handler: (event: MouseEvent) => void
+) => {
+  const ref = useRef<T>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        handler(event);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [handler]);
+
+  return ref;
+};
